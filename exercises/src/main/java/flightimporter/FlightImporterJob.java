f324a757814692bab90b581f1c04ba5893c4a458
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-09-14 22:25:10 +0900

    first flink job

---
+++ exercises/src/main/java/flightimporter/FlightImporterJob.java
@@ -0,0 +1,11 @@
+import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
+import org.apache.flink.connector.kafka.source.KafkaSource;
+import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class FlightImporterJob
