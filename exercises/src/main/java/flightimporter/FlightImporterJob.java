0e8d55e52b5c764984d91da7916b18c33665626b
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-09-16 21:59:33 +0900

    Creating a Flink Data Sink

--- exercises/src/main/java/flightimporter/FlightImporterJob.java
+++ exercises/src/main/java/flightimporter/FlightImporterJob.java
@@ -1,30 +1,36 @@
 package flightimporter;
 
-import models.FlightData;
 import models.SkyOneAirlinesFlightData;
+import models.FlightData;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
+import org.apache.flink.connector.kafka.sink.KafkaSink;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.formats.json.JsonDeserializationSchema;
+import org.apache.flink.formats.json.JsonSerializationSchema;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 
-import java.io.InputStream;
 import java.time.ZonedDateTime;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class FlightImporterJob {
 
-    public static DataStream<FlightData> defineWorkflow(DataStream<SkyOneAirlinesFlightData> skyOneSource) {
-        return skyOneSource.filter(skyOneAirlinesFlightData -> skyOneAirlinesFlightData.getFlightArrivalTime().isAfter(ZonedDateTime.now()))
-                .map(skyOneAirlinesFlightData -> skyOneAirlinesFlightData.toFlightData());
-    }
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         Properties consumerConfig = new Properties();
         try (InputStream stream = FlightImporterJob.class.getClassLoader().getResourceAsStream("consumer.properties")) {
-            consumerConfig.load(stream);
+                consumerConfig.load(stream);
+        }
+
+        Properties producerConfig = new Properties();
+        try (InputStream stream = FlightImporterJob.class.getClassLoader().getResourceAsStream("producer.properties")) {
+                producerConfig.load(stream);
         }
 
         KafkaSource<SkyOneAirlinesFlightData> skyOneSource = KafkaSource.<SkyOneAirlinesFlightData>builder()
@@ -37,9 +43,31 @@ public class FlightImporterJob {
         DataStream<SkyOneAirlinesFlightData> skyOneStream = env
             .fromSource(skyOneSource, WatermarkStrategy.noWatermarks(), "skyone_source");
 
-        DataStream<FlightData>  flightDataStream = defineWorkflow(skyOneStream);
-        flightDataStream.print();
+        KafkaRecordSerializationSchema<FlightData> flightSerializer = KafkaRecordSerializationSchema.<FlightData>builder()
+            .setTopic("flightdata")
+            .setValueSerializationSchema(new JsonSerializationSchema<FlightData>(
+                () -> {
+                    return new ObjectMapper()
+                        .registerModule(new JavaTimeModule());
+                }
+            ))
+            .build();
+
+        KafkaSink<FlightData> flightSink = KafkaSink.<FlightData>builder()
+            .setKafkaProducerConfig(producerConfig)
+            .setRecordSerializer(flightSerializer)
+            .build();
+
+        defineWorkflow(skyOneStream)
+            .sinkTo(flightSink)
+            .name("flightdata_sink");
 
         env.execute("FlightImporter");
+    }
+
+    public static DataStream<FlightData> defineWorkflow(DataStream<SkyOneAirlinesFlightData> skyOneSource) {
+        return skyOneSource
+            .filter(flight -> flight.getFlightArrivalTime().isAfter(ZonedDateTime.now()))
+            .map(flight -> flight.toFlightData());
     }
 }
