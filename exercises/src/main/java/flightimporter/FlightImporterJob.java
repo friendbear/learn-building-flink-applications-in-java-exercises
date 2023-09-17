395b5219113ea5c3d7b95c12a3e8c91677a1bdab
T Kumagai <4956799+friendbear@users.noreply.github.com>
2023-09-17 18:48:48 +0900

    Merging Flink Data Streams.

--- exercises/src/main/java/flightimporter/FlightImporterJob.java
+++ exercises/src/main/java/flightimporter/FlightImporterJob.java
@@ -2,6 +2,7 @@ package flightimporter;
 
 import models.SkyOneAirlinesFlightData;
 import models.FlightData;
+import models.SunsetAirFlightData;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import org.apache.flink.connector.kafka.sink.KafkaSink;
@@ -40,8 +41,18 @@ public class FlightImporterJob {
             .setValueOnlyDeserializer(new JsonDeserializationSchema(SkyOneAirlinesFlightData.class))
             .build();
 
+        KafkaSource<SunsetAirFlightData> sunsetSource = KafkaSource.<SkyOneAirlinesFlightData>builder()
+            .setProperties(consumerConfig)
+            .setTopics("sunset")
+            .setStartingOffsets(OffsetsInitializer.latest())
+            .setValueOnlyDeserializer(new JsonDeserializationSchema(SunsetAirFlightData.class))
+            .build();
+
         DataStream<SkyOneAirlinesFlightData> skyOneStream = env
             .fromSource(skyOneSource, WatermarkStrategy.noWatermarks(), "skyone_source");
+
+        DataStream<SunsetAirFlightData> sunsetStream = env
+            .fromSource(sunsetSource, WatermarkStrategy.noWatermarks(), "sunset_source");
 
         KafkaRecordSerializationSchema<FlightData> flightSerializer = KafkaRecordSerializationSchema.<FlightData>builder()
             .setTopic("flightdata")
@@ -58,16 +69,22 @@ public class FlightImporterJob {
             .setRecordSerializer(flightSerializer)
             .build();
 
-        defineWorkflow(skyOneStream)
+        defineWorkflow(skyOneStream, sunsetStream)
             .sinkTo(flightSink)
             .name("flightdata_sink");
 
         env.execute("FlightImporter");
     }
 
-    public static DataStream<FlightData> defineWorkflow(DataStream<SkyOneAirlinesFlightData> skyOneSource) {
-        return skyOneSource
+    public static DataStream<FlightData> defineWorkflow(DataStream<SkyOneAirlinesFlightData> skyOneSource, DataStream<SunsetAirFlightData> sunsetSource) {
+        DataStream<FlightData> skyOneFlightStream = skyOneSource
             .filter(flight -> flight.getFlightArrivalTime().isAfter(ZonedDateTime.now()))
             .map(flight -> flight.toFlightData());
+
+        DataStream<FlightData> sunsetFlightStream = sunsetSource
+            .filter(flight -> flight.getArrivalTime().isAfter(ZonedDateTime.now()))
+            .map(flight -> flight.toFlightData());
+
+        return skyOneFlightStream.union(sunsetFlightStream);
     }
 }
